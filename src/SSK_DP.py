275ab0044d75931faacc22629a8ef0abfb45fa68
Tim Lachmann <Tim@Lachmann.nu>
2018-01-09 15:28:22 +0100

    added SSK_DP with clear variable names and less clutter

--- src/SSK_DP.py
+++ src/SSK_DP.py
@@ -3,89 +3,86 @@
 import numpy as np
 from math import sqrt
 
+
 # K''_i(s,t)
 def kdoubleprime(s, t, i):
+    if min(len(s), len(t)) < i:
+        return 0
 
-	if(min(len(s), len(t)) < i):
-		return 0;
-	
-	x = s[-1];
-	z = t[-1];
-	
-	if(x == z):
-		res = lambdaval*(kdoubleprime(s, t[0:-1], i) + lambdaval * kprime(s[0:-1], t[0:-1], i-1))
-		return res;
-	else:
-		#k__ = doubleprime(s, t[0:-1])
-		k__ = 0;
-		j = [] #	j:tj = x
-		for m in range(len(t)):
-			if(t[m] == x):
-				j.append(m);
+    x = s[-1]
+    z = t[-1]
 
-		for j_ in j:			
-			k__ += kprime(s, t[0:j_], i-1) * lambdaval**(len(t) - j_ + 2);
+    if x == z:
+        res = lambdaval * (kdoubleprime(s, t[0:-1], i) + lambdaval * kprime(s[0:-1], t[0:-1], i - 1))
+        return res
+    else:
+        # k__ = doubleprime(s, t[0:-1])
+        k__ = 0
+        j = []  # j:tj = x
+        for m in range(len(t)):
+            if t[m] == x:
+                j.append(m)
 
-		return k__
-	
+        for j_ in j:
+            k__ += kprime(s, t[0:j_], i - 1) * lambdaval ** (len(t) - j_ + 1)
+
+        return k__
+
 
 # K'_i(s,t)
 def kprime(s, t, i):
-	if(i <= 0):
-		return 1;
+    if i <= 0:
+        return 1
 
-	if(min(len(s), len(t)) < i):
-		return 0;
+    if min(len(s), len(t)) < i:
+        return 0
 
-	exp1 = lambdaval * kprime(s[0:-1], t, i)
-	exp2 = kdoubleprime(s, t, i)
-	
-	return (exp1 + exp2);
+    exp1 = lambdaval * kprime(s[0:-1], t, i)
+    exp2 = kdoubleprime(s, t, i)
+
+    return (exp1 + exp2)
+
 
 # K_i(s, t) = inner product between s & t
 # i = length of substring
 def k(s, t, i):
-	if(min(len(s), len(t)) < i):
-		return 0;
-		
-	x = s[-1];
-		
-	exp1 = k(s[0:-1], t, i) 
-	exp2 = 0
+    if (min(len(s), len(t)) < i):
+        return 0
 
-	j = [] #	j:tj = x
-	for m in range(len(t)):
-		if(t[m] == x):
-			j.append(m);
-	for j_ in j:
-		exp2 += kprime(s[0:-1], t[0:j_], i-1) * lambdaval**2;
-	return (exp1 + exp2);
+    x = s[-1]
+
+    exp1 = k(s[0:-1], t, i)
+    exp2 = 0
+
+    j = []  # j:tj = x
+    for m in range(len(t)):
+        if t[m] == x:
+            j.append(m)
+    for j_ in j:
+        exp2 += kprime(s[0:-1], t[0:j_], i - 1) * lambdaval ** 2
+    return (exp1 + exp2)
+
 
 def normk(s, t, i):
-	k1 = k(s,s,i)
-	k2 = k(t,t,i)
-	res = k(s,t, i) / sqrt(k1*k2)
-	return res;
-	
-
-lambdaval = 0.5; # decay factor - penalizes non-contiguous substrings, value between 0 and 1
+    k1 = k(s, s, i)
+    k2 = k(t, t, i)
+    res = k(s, t, i) / sqrt(k1 * k2)
+    return res
 
 
+lambdaval = 0.5  # decay factor - penalizes non-contiguous substrings, value between 0 and 1
 
 print('k(cat, car, 2) = ', k('cat', 'car', 2), 'should be lambda^4 = 0.0625')
 print('k(car, car, 2) = ', k('car', 'car', 2), ' should be lambda^6 + 2*lambda^4 = 0.140625')
 print('normk(cat, car, 2) = ', normk('cat', 'car', 2), 'should be 1/(2+lambda^2) = 0.44444')
-#print('k(ca, car, 2) = ', k('ca', 'car', 2))
-#print('k(cat, car, 1) = ', k('cat', 'car', 1))
+# print('k(ca, car, 2) = ', k('ca', 'car', 2))
+# print('k(cat, car, 1) = ', k('cat', 'car', 1))
 
 
 kss = [0.580, 0.580, 0.478, 0.439, 0.406, 0.370]
 
-#for x in range(1,7):
-#	print(x, normk("science is organized knowledge", "wisdom is organized life", x), 'should be', kss[x-1])
-	
-
-
+for x in range(1,7):
+	print(x, normk("science is organized knowledge", "wisdom is organized life", x), 'should be', kss[x-1])
 
 
 
--- src/SSK_memo.py
+++ src/SSK_memo.py
@@ -4,10 +4,9 @@
 import numpy as np
 from math import sqrt
 
+lambdaval = 0.5  # decay factor - penalizes non-contiguous substrings, value between 0 and 1
 
-lambdaval = 0.5; # decay factor - penalizes non-contiguous substrings, value between 0 and 1
-
-#print('k(cat, car, 2) = ', k('cat', 'car', 2))
+# print('k(cat, car, 2) = ', k('cat', 'car', 2))
 
 s = "cat"
 t = "car"
@@ -15,43 +14,38 @@ n = 2
 
 #	compute k(cat, car, 2) with memoization - ¡might be more efficient ways to do this but it was the first i thought of!
 
-kdoubleprimes = np.zeros((n, len(s)+1, len(t)+1))
-kprimes = np.zeros((n, len(s)+1, len(t)+1))
+kdoubleprimes = np.zeros((n, len(s) + 1, len(t) + 1))
+kprimes = np.zeros((n, len(s) + 1, len(t) + 1))
 
 # 1st row
-for s_ in range(len(s)+1):
-	for t_ in range(len(t)+1):
-		kprimes[0][s_][t_] = 1;
+for s_ in range(len(s) + 1):
+    for t_ in range(len(t) + 1):
+        kprimes[0][s_][t_] = 1
 
 for n_ in range(1, n):
-	for s_ in range(len(s)+1):
-		for t_ in range(len(t)+1):
-			print(s_, t_, s[0:s_-1], t[0:t_-1], '*')
-			if(min(s_, t_) < n_):
-				kdoubleprimes[n_][s_][t_] = 0;
-				kprimes[n_][s_][t_] = 0;				
-			else:
-				kdoubleprimes[n_][s_][t_] = lambdaval * kdoubleprimes[n_][s_][t_-1] + lambdaval**2 * kprimes[n_-1][s_-1][t_-1]
-				kprimes[n_][s_][t_] = lambdaval * kprimes[n_][s_-1][t_] + kdoubleprimes[n_][s_][t_];
+    for s_ in range(len(s) + 1):
+        for t_ in range(len(t) + 1):
+            print(s_, t_, s[0:s_ - 1], t[0:t_ - 1], '*')
+            if (min(s_, t_) < n_):
+                kdoubleprimes[n_][s_][t_] = 0
+                kprimes[n_][s_][t_] = 0
+            else:
+                kdoubleprimes[n_][s_][t_] = lambdaval * kdoubleprimes[n_][s_][t_ - 1] + lambdaval ** 2 * \
+                                            kprimes[n_ - 1][s_ - 1][t_ - 1]
+                kprimes[n_][s_][t_] = lambdaval * kprimes[n_][s_ - 1][t_] + kdoubleprimes[n_][s_][t_]
 
-
-ks = np.zeros((len(s)+1, len(t)+1)) # K_2(s,t)
-for s_ in range(len(s)+1):
-	for t_ in range(len(t)+1):
-		if(min(s_, t_) < n_):
-			ks[s_][t_] = 0;
-		else:
-			x = s[min(len(s)-1, s_-1)] # last letter
-			j = [pos for pos, char in enumerate(s) if char == x]
-			ks[s_][t_] = ks[s_-1][t_] + lambdaval**2 * sum([kprimes[n-1][s_-1][t_- j_] for j_ in j]);
-
-
+ks = np.zeros((len(s) + 1, len(t) + 1))  # K_2(s,t)
+for s_ in range(len(s) + 1):
+    for t_ in range(len(t) + 1):
+        if (min(s_, t_) < n_):
+            ks[s_][t_] = 0
+        else:
+            x = s[min(len(s) - 1, s_ - 1)]  # last letter
+            j = [pos for pos, char in enumerate(s) if char == x]
+            ks[s_][t_] = ks[s_ - 1][t_] + lambdaval ** 2 * sum([kprimes[n - 1][s_ - 1][t_ - j_] for j_ in j])
 
 print(kdoubleprimes, 'kdoubleprimes')
 print(kprimes, 'kprimes')
 print(ks, 'ks')
 
-
 print('k(ca, car, 2) = ', ks[-1][-1], 'unnormalized')
-
-
