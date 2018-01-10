f01f6b23cfc4e10323a827ccf48d1d9ced197370
Arvid <arvidspost@gmail.com>
2018-01-10 14:38:21 +0100

    Use sparse matrix representation

--- src/wk.py
+++ src/wk.py
@@ -13,40 +13,35 @@ n: total number of documents.
 
 The documents are normalised so that each document has equal length.
 '''
-import numpy as np
 from collections import Counter
 from math import log
+from scipy.sparse import lil_matrix
+from sklearn.preprocessing import normalize
 
 
 def create_doc_word_matrix(texts, N):
     corpus = []
     n = len(texts)
     df = Counter()
-    doc_vecs = {}
+    doc_map = {}
     for text in texts.values():
         words = text.split(' ')
         corpus.extend(words)
         df.update(words)
     most_common_words = Counter(corpus).most_common(N)
     word_set = set([word for word, _ in most_common_words])
-    mat = np.zeros((n, N))
+    word_map = {word: index for index, word in enumerate(word_set)}
+    mat = lil_matrix((n, N))
     for i, d in enumerate(texts.items()):
         document_id, text = d
         words = text.split(' ')
         tf = Counter([word for word in words if word in word_set])
-        for j, t in enumerate(most_common_words):
-            word, _ = t
-            if word in tf:
-                mat[i, j] = tfidf(tf[word], df[word], n)
-        doc_vecs[document_id] = i
-        return doc_vecs, mat
+        for word in tf:
+            j = word_map[word]
+            mat[i, j] = tfidf(tf[word], df[word], n)
+        doc_map[document_id] = i
+    return doc_map, normalize(mat.tocsr())
 
 
 def tfidf(tf, df, n):
     return log(1 + tf) * log(n / df)
-
-
-def normalize(X):
-    r, _ = X.shape
-    norms = np.linalg.norm(X, axis=1, ord=2)
-    return X / norms.reshape(r, 1)
